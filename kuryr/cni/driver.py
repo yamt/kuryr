34bf76b6eb31608583331850b2711717ef9bb351
Jaume Devesa <devvesa@gmail.com>
2016-03-08 18:10:40 +0000

    Introducing the CNI driver
    Add the CNI driver for K8s. The structure proposed tries to be flexible
    enough to add an eventual CNI+Rocket.
    
    CNI driver with K8s retrieves the Neutron port and subnets in
    "annotations" property of the pod metadata from the K8s API for binding
    the created veth endpoint. This data is introduced by Raven watchers.
    
    Signed-off-by: Jaume Devesa <devvesa@gmail.com>
    Signed-off-by: Taku Fukushima <f.tac.mac@gmail.com>
    Signed-off-by: Antoni Segura Puimedon <toni@midokura.com>
    
    Change-Id: I998a1dfe7739fc6c361a6e8a220e1dfce4bc1dae
    Implements: task/70

---
+++ kuryr/cni/driver.py
@@ -0,0 +1,172 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Kuryr CNI Driver.
+
+This module provides the abstract methods and entry points to implement
+the CNI spec: https://github.com/appc/cni
+"""
+
+import abc
+import os
+
+import netaddr
+import requests
+
+from oslo_concurrency import processutils
+from oslo_config import cfg
+from oslo_log import log as logging
+from oslo_serialization import jsonutils
+from oslo_utils import excutils
+
+from kuryr._i18n import _LE
+from kuryr import binding
+from kuryr.cni import constants
+from kuryr.cni import models
+from kuryr.common import exceptions
+
+
+CONF = cfg.CONF
+LOG = logging.getLogger(__name__)
+
+
+class KuryrCNIDriver(object):
+    """Abstracted CNI driver interface.
+
+    This class requires the users to implement add and delete methods.
+    """
+    MANDATORY_ENV_VARS = frozenset([
+        constants.PATH, constants.CONTAINERID, constants.NETNS,
+        constants.IFNAME, constants.COMMAND, constants.ARGS])
+
+    def __init__(self):
+        self.env = os.environ.copy()
+        self._check_envvars()
+        self.path = self.env[constants.PATH]
+        self.container_id = self.env[constants.CONTAINERID]
+        self.netns = self.env[constants.NETNS]
+        self.ifname = self.env[constants.IFNAME]
+        self.command = self.env[constants.COMMAND]
+        self.cni_args = self._parse_cni_args_as_dict(
+            self.env[constants.ARGS])
+        LOG.debug('%s %s %s %s %s', self.path, self.container_id, self.netns,
+                  self.ifname, self.command)
+
+    @abc.abstractmethod
+    def add(self):
+        """Adds an interface."""
+
+    @abc.abstractmethod
+    def delete(self):
+        """Deletes an interface."""
+
+    def run_command(self):
+        """Runs the command defined by CNI_COMMAND, either "ADD" or "DEL"."""
+        if self.command == constants.METHOD_ADD:
+            return self.add()
+        if self.command == constants.METHOD_DEL:
+            return self.delete()
+
+    def _check_envvars(self):
+        for var in self.MANDATORY_ENV_VARS:
+            if var not in self.env:
+                msg_error = "Missing ENV VAR '%s'" % var
+                kuryr_error = models.CNIError(1, msg_error)
+                LOG.error(_LE('Lacking environment %s'), kuryr_error)
+                raise exceptions.KuryrException(str(kuryr_error))
+
+    def _parse_cni_args_as_dict(self, cni_args):
+        """Returns the resulting dictionary from parsing cni args (can be {})
+
+        Turns FOO=A;BAR=B;BELL=BAR
+        into {'BAR': 'B', 'BELL': 'BAR', 'FOO': 'A'}
+        """
+        return {key: value for key, value in (
+                item.split('=') for item in cni_args.split(';') if item)}
+
+
+class KuryrCNIK8sNeutronDriver(KuryrCNIDriver):
+    """CNI Driver for K8s that binds Neutron ports."""
+    def __init__(self):
+        super(KuryrCNIK8sNeutronDriver, self).__init__()
+        self.pod_namespace = self.cni_args['K8S_POD_NAMESPACE']
+        self.pod_name = self.cni_args['K8S_POD_NAME']
+        self.pod_container_id = self.cni_args['K8S_POD_INFRA_CONTAINER_ID']
+
+    def _get_pod_annotaions(self):
+        k8s_url = "%s/api/v1/namespaces/%s/pods/%s" % (CONF.k8s.api_root,
+            self.pod_namespace, self.pod_name)
+        LOG.debug('URL of the pod %s', k8s_url)
+        response = requests.get(k8s_url)
+        pod = response.json()
+        return pod['metadata']['annotations']
+
+    def add(self):
+        """Binds the container to the Neutron port given by the API watcher."""
+        LOG.debug('Binding the pod %s into network', self.container_id)
+
+        endpoint_id = self.container_id
+        pod_annotations = self._get_pod_annotaions()
+        neutron_port = jsonutils.loads(pod_annotations[constants.NEUTRON_PORT])
+        neutron_subnets = jsonutils.loads(
+            pod_annotations[constants.NEUTRON_SUBNETS])
+
+        try:
+            ifname, peer_name, (stdout, stderr) = binding.port_bind(
+                endpoint_id, neutron_port, neutron_subnets,
+                ifname=self.ifname, netns=self.netns)
+            LOG.debug(stdout)
+            if stderr:
+                LOG.error(stderr)
+        except exceptions.VethCreationFailure as ex:
+            with excutils.save_and_reraise_exception():
+                LOG.error(_LE('Preparing the veth pair was failed: {0}.')
+                          .format(ex))
+        except processutils.ProcessExecutionError:
+            with excutils.save_and_reraise_exception():
+                LOG.error(_LE(
+                    'Could not bind the Neutron port to the veth endpoint.'))
+
+        info = models.CNIInfo()
+        cidr = netaddr.IPNetwork(neutron_subnets[0]['cidr'])
+        port_ip = '/'.join([pod_annotations['NEUTRON_IP'],
+                            str(cidr.prefixlen)])
+        gateway_ip = neutron_subnets[0]['gateway_ip']
+        info.set_ipv4_info(ip=port_ip, gateway=gateway_ip)
+        LOG.debug(str(info))
+        return str(info)
+
+    def delete(self):
+        """Unbinds the Neutron port from the pod."""
+        endpoint_id = self.container_id
+        pod_annotations = self._get_pod_annotaions()
+        neutron_port = jsonutils.loads(pod_annotations[constants.NEUTRON_PORT])
+
+        try:
+            stdout, stderr = binding.port_unbind(endpoint_id, neutron_port)
+            LOG.debug(stdout)
+            if stderr:
+                LOG.error(stderr)
+        except processutils.ProcessExecutionError:
+            with excutils.save_and_reraise_exception():
+                LOG.error(_LE(
+                    'Could not unbind the Neutron port from the veth '
+                    'endpoint.'))
+        except exceptions.VethDeletionFailure:
+            with excutils.save_and_reraise_exception():
+                LOG.error(_LE('Cleaning the veth pair up was failed.'))
+
+
+def run_driver():
+    """Runs the CNI driver."""
+    driver = KuryrCNIK8sNeutronDriver()
+    return driver.run_command()
---
+++ kuryr/cni/models.py
@@ -0,0 +1,98 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Kuryr CNI Models.
+
+CNI Message returns modeled in python classes for a better manipulation
+"""
+
+from oslo_serialization import jsonutils
+
+
+class CNIError(object):
+    """Message error for CNI response"""
+    def __init__(self, code, msg, details=''):
+        self.code = code
+        self.msg = msg
+        self.details = details
+
+    def __repr__(self):
+        error = {}
+        error['cniVersion'] = '0.1.0'
+        error['code'] = self.code
+        error['msg'] = self.msg
+        error['details'] = self.details
+        error_json = jsonutils.dumps(error)
+        return error_json
+
+
+class CNIInfo(object):
+    """CNI information returned to Kubelet through stdout.
+
+    This is the class representation of the JSON data returned to Kubelet. See
+    the following link for more details.
+
+    - https://github.com/appc/cni/blob/master/SPEC.md#result
+    """
+    def __init__(self):
+        self.ipv4_ip = ''
+        self.ipv4_gateway = ''
+        self.ipv4_routes = []
+        self.ipv6_ip = ''
+        self.ipv6_gateway = ''
+        self.ipv6_routes = []
+        self.dns_nameservers = []
+        self.dns_domain = ''
+        self.dns_search = []
+        self.dns_options = []
+
+    def set_ipv4_info(self, ip='', gateway='', routes=None):
+        self.ipv4_ip = ip
+        self.ipv4_gateway = gateway
+        self.ipv4_routes = routes or []
+
+    def set_ipv6_info(self, ip='', gateway='', routes=None):
+        self.ipv6_ip = ip
+        self.ipv6_gateway = gateway
+        self.ipv6_routes = routes or []
+
+    def set_dns_info(self, nameservers=None, domain='',
+                     search=None, options=None):
+        self.dns_nameservers = nameservers or []
+        self.dns_domain = domain
+        self.dns_search = search or []
+        self.dns_options = options or []
+
+    def __repr__(self):
+        info = {}
+        info['cniVersion'] = '0.1.0'
+
+        info_ipv4 = {}
+        info_ipv4['ip'] = self.ipv4_ip
+        info_ipv4['gateway'] = self.ipv4_gateway
+        info_ipv4['routes'] = self.ipv4_routes
+        info['ip4'] = info_ipv4
+
+        # info_ipv6 = {}
+        # info_ipv6['ip'] = self.ipv6_ip
+        # info_ipv6['gateway'] = self.ipv6_gateway
+        # info_ipv6['routes'] = self.ipv6_routes
+        # info['ip6'] = info_ipv6
+
+        info_dns = {}
+        info_dns['nameservers'] = self.dns_nameservers
+        info_dns['domain'] = self.dns_domain
+        info_dns['search'] = self.dns_search
+        info_dns['options'] = self.dns_options
+        info['dns'] = info_dns
+
+        return jsonutils.dumps(info)
